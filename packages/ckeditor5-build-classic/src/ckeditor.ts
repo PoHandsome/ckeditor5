9fc468205159ab60b0f5855c10e1b3af9c616cc3
Howard Chen <howard.chen@pixelforce.com.au>
2023-03-09 11:23:40 +1030

    fix

--- packages/ckeditor5-build-classic/src/ckeditor.ts
+++ packages/ckeditor5-build-classic/src/ckeditor.ts
@@ -101,6 +101,5 @@ ClassicEditor.defaultConfig = {
 		]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en',
-	plugins: [ SimpleUploadAdapter ]
+	language: 'en'
 };
